5945b06fef9b999ee9d4aa36531e98a6e111470a
Jeremy Weiss <18493608+jweiss045@users.noreply.github.com>
2018-10-18 00:09:25 -0400

    Made header logo and tagline mobile responsive

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,8 @@
 $(function () {
   $(document).scroll(function () {
 	  var $nav = $(".navbar-fixed-top");
+    var $nav_brand = $(".navbar-brand")
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav_brand.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
